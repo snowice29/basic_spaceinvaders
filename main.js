68d656abcc6a88a6571f043e7b76d9ec47477aed
snowice29 <hamxa.waseem3@gmail.com>
2019-04-14 12:30:39 -0500

    canvas fix

---
+++ main.js
@@ -0,0 +1,125 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+
+var game = { score: 0, lives: 1, state: "play", level: 1 };
+
+var gameObjects = [];
+
+gameObjects.push({ type: "player", x: 20, y: c.height / 2, r: 15, color: "green", speed: 10 });
+
+gameObjects.push({ type: "harm", x: 100, y: 75, r: 10, color: "red", speed: 1 });
+
+gameObjects.push({ type: "benefit", x: 20, y: 25, r: 20, color: "yellow", speed: 1 });
+
+var player = gameObjects[0];
+
+var backgroundImg = new Image();
+backgroundImg.src = "game-bg.png";
+
+ctx.font = "25px QuickSand";
+ctx.fillStyle = "red";
+ctx.textAlign = "center";
+
+function draw() {
+    // clear canvas
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.drawImage(backgroundImg, 0, 0);
+
+    // draw objects
+    for (idx in gameObjects) {
+        g = gameObjects[idx];
+        ctx.beginPath();
+        ctx.arc(g.x, g.y, g.r, 0, 2 * Math.PI);
+        //ctx.stroke();
+        ctx.fillStyle = g.color;
+        ctx.fill();
+
+        if (g.type != "player") {
+
+            g.x -= g.speed * game.level;
+
+            if (colliding(player, g)) {
+                if (g.type == "harm") {
+                    game.lives--;
+                } else if (g.type == "benefit") {
+                    game.score += 100;
+
+                    if (game.score % 100 == 0) {
+                        game.level++;
+
+                        if (game.level % 2 == 0) {
+                            gameObjects.push({ type: "harm", x: 100, y: 75, r: 10, color: "red", speed: 1 });
+                        }
+                    }
+                }
+                console.log(game);
+
+                g.state = "collision"
+            }
+            if ((g.x + g.r < 0) || g.state == "collision") {
+                g.x = c.width + g.r;
+                g.y = Math.random() * (c.height - 0) + 0;
+                g.state = "";
+            }
+        }
+    }
+
+    // draw text
+    ctx.fillText("Score: " + game.score + " - Lives: " + game.lives, c.width / 2, 30);
+
+    // next frame
+    if (game.lives > 0) {
+        window.requestAnimationFrame(draw);
+    } else {
+        gameOver();
+    }
+
+}
+
+function gameOver() {
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.drawImage(backgroundImg, 0, 0);
+    ctx.fillText("Game Over", c.width / 2, c.height / 2);
+
+}
+
+window.requestAnimationFrame(draw);
+
+function colliding(circle1, circle2) {
+    var result = false;
+
+    var dx = circle1.x - circle2.x;
+    var dy = circle1.y - circle2.y;
+    var distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < circle1.r + circle2.r) {
+        // collision detected!
+        result = true;
+    }
+    return result;
+}
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    // console.log(e);
+    if (e.keyCode == '38') {
+        // up arrow
+        player.y -= player.speed;
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+        player.y += player.speed;
+    }
+    else if (e.keyCode == '37') {
+        // left arrow
+    }
+    else if (e.keyCode == '39') {
+        // right arrow
+    }
+
+}
